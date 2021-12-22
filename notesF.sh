ddfdc63e46235b9390da15251c54cba3a7f7e8e4
Pat-Lang <patrick.langlois@umontreal.ca>
2021-12-22 17:06:13 -0500

    V3.0

--- notesF.sh
+++ notesF.sh
@@ -5,29 +5,20 @@ notesF ()
  {
 while IFS="," read -ers  prenom nom ID TP1 TP2 TP3 TP4 Intra Final extra
   do
-    echo "i got : $prenom|$nom |$ID |$TP1 |$TP2 |$TP3 |$TP4|$Intra |$Final"
     IetF 25 35
-    echo "pond: " $pond
-    echo "intra: " $Intra
-    echo "Final: " $Final
-    Exam $Intra $Final
-    echo "noteExam: "$noteExam
+    Exam "$Intra" "$Final"
     noteTp "$TP1" "$TP2" "$TP3" "$TP4"
-    echo "noteTp:" "$noteTp"
    if (( $(bc <<<"$noteExam > $pond") )); then 
-   echo "pass"
-    total=$( echo "scale=2;$noteTp+$noteExam" | bc)
-    echo "total: "$total
-    echo " final : $prenom| $nom| $ID| $TP1| $TP2| $TP3| $TP4| $noteExam |$total"
-    echo " "
+      total=$( echo "scale=2;$noteTp+$noteExam" | bc)
+      echo " final : $prenom| $nom| $ID| $TP1| $TP2| $TP3| $TP4| $noteExam |$total"
+      echo " "
    else
-   echo "fail"
-     noteTp $TP1 $TP2 $TP3 $TP4
+      noteTp $TP1 $TP2 $TP3 $TP4
       total=$( echo "scale=2; $noteExam+($noteTp/2)" | bc)
       echo "final: $prenom|$nom |$ID |$TP1 |$TP2 |$TP3 |$TP4 |$Intra |$Final |$noteExam |$total"
       echo " "
     
-    fi
+   fi
      
 done < "$1"
 echo "end"
@@ -39,7 +30,6 @@ IetF()
 {
      z=$1+$2
      pond=$( echo "scale=2; ($z)/2" | bc)
-    #echo $pond
 }
 noteTp ()
 {
@@ -54,4 +44,5 @@ Exam ()
 {
 noteExam=$( echo "scale=2; $Intra*25/100+$Final*35/100" | bc)
 }
+
 notesF listeNotes.csv
